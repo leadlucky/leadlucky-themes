be44174452bea195fbb09dffc28239c6ee3c1ef1
trevor1400e <thuff2015@gmail.com>
2019-01-06 20:42:02 -0600

    fixed the mixin bugs

--- src/mixins/validationMixin.js
+++ src/mixins/validationMixin.js
@@ -29,62 +29,59 @@ function buildRules(regExps = [], fns = [], errorMessages = ["Input is invalid"]
   }]
 }
 
+const mixin = {}
+  mixin.emailRules = buildRules(
+    [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/],
+    [],
+    ['Invalid email']
+  );
 
-export default {
-  created(){
-    this.emailRules = buildRules(
-      [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/],
-      [],
-      ['Invalid email']
-    );
+  mixin.userIdRules = buildRules(
+  [/^[A-Za-z0-9]*$/],
+  [],
+  ['Invalid username']
+  );
 
-    this.userIdRules = buildRules(
-      [/^[A-Za-z0-9]*$/],
-      [],
-      ['Invalid username']
-    );
+  mixin.passwordRules = buildRules(
+  [],
+  [
+    pwd => pwd.length >= 8,
+    pwd => /.*[A-Z].*/.test(pwd) && /.*[a-z].*/,
+    pwd => /.*[0-9].*/.test(pwd),
+    pwd => /.*[!@#$%^&*()\-_=+[{}\]| /\\.,<>`~'";:?].*/.test(pwd)
+  ],
+  [
+    'You must create a password',
+    'Password must be at least 8 characters',
+    'Password must contain upper and lowercase letters',
+    'Password must contain a number',
+    'Password must contain a special character',
+  ]
+);
 
-    this.passwordRules = buildRules(
-      [],
-      [
-        pwd => pwd.length >= 8,
-        pwd => /.*[A-Z].*/.test(pwd) && /.*[a-z].*/,
-        pwd => /.*[0-9].*/.test(pwd),
-        pwd => /.*[!@#$%^&*()\-_=+[{}\]| /\\.,<>`~'";:?].*/.test(pwd)
-      ],
-      [
-        'You must create a password',
-        'Password must be at least 8 characters',
-        'Password must contain upper and lowercase letters',
-        'Password must contain a number',
-        'Password must contain a special character',
-      ]
-    );
+mixin.required = (fieldName) => buildRules([],[],[`${fieldName} is required`]);
 
-    this.required = (fieldName) => buildRules([],[],[`${fieldName} is required`]);
+mixin.checkedRule = (message) => buildRules([],[],[message]);
 
-    this.checkedRule = (message) => buildRules([],[],[message]);
+mixin.passwordsMatch = (originalPassword) => buildRules(
+  [],[(validatePassword) => validatePassword === originalPassword],
+  ['Passwords must match']
+);
 
-    this.passwordsMatch = (originalPassword) => buildRules(
-      [],[(validatePassword) => validatePassword === originalPassword],
-      ['Passwords must match']
-    );
+mixin.urlRule = buildRules(
+  [],
+  [(url) => {
+    if(url.toLowerCase().startsWith('http://')){
+      url = url.substring('http://'.length)
+    }
+    if(url.toLowerCase().startsWith('https://')){
+      url = url.substring('https://'.length)
+    }
+    return /[A-z|0-9]+\.[A-z|0-9]{2,22}/.test(url)
+  }],
+  ['Invalid URL']
+);
 
-    this.urlRule = buildRules(
-      [],
-      [(url) => {
-        if(url.toLowerCase().startsWith('http://')){
-          url = url.substring('http://'.length)
-        }
-        if(url.toLowerCase().startsWith('https://')){
-          url = url.substring('https://'.length)
-        }
-        return /[A-z|0-9]+\.[A-z|0-9]{2,22}/.test(url)
-    }],
-      ['Invalid URL']
-    );
+mixin.pageIdRule = buildRules([/^[A-Za-z0-9]*$/], [], 'Invalid Page Id')
 
-    this.pageIdRule = buildRules([/^[A-Za-z0-9]*$/], [], 'Invalid Page Id')
-
-  }
-}
+export default mixin
